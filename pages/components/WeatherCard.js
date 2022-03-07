41205af712f89ac904bef36c03d3d13da160327b
Hatim Amtil Ouahdi <hatim.amtil>
2022-03-07 02:11:11 +0100

    fix build

--- pages/components/WeatherCard.js
+++ pages/components/WeatherCard.js
@@ -3,32 +3,33 @@ import { toCelsius } from "../../helpers/format";
 
 export default function WeatherCard(props) {
   const { data = {} } = props;
+  const { weather = [], main = {}, clouds = {}, wind = {}, dt_txt = "" } = data;
 
   return (
     <div className="text-center p-3 border border-solid border-indigo-600 rounded hover:cursor-pointer hover:text-indigo-600">
       <Image
         className="mx-auto"
         src={`http://openweathermap.org/img/wn/${
-          ((data.weather || [])[0] || {}).icon
+          (weather[0] || {}).icon
         }@2x.png`}
-        alt={((data.weather || [])[0] || {}).description}
+        alt={(weather[0] || {}).description}
         height={100}
         width={100}
       ></Image>
-      <p>{new Date(data.dt_txt).toLocaleDateString()}</p>
-      <p className="font-medium text-4xl">{toCelsius(data.main.temp)} ºC</p>
+      <p>{new Date(dt_txt).toLocaleDateString()}</p>
+      <p className="font-medium text-4xl">{toCelsius(main.temp)} ºC</p>
       <p>
-        <span className="font-normal">{toCelsius(data.main.temp_min)}</span> |{" "}
-        <span className="font-light">{toCelsius(data.main.temp_max)}</span>
+        <span className="font-normal">{toCelsius(main.temp_min)}</span> |{" "}
+        <span className="font-light">{toCelsius(main.temp_max)}</span>
       </p>
       <p>
-        <span className="font-normal">Humedad:</span> {data.main.humidity}
+        <span className="font-normal">Humedad:</span> {main.humidity}
       </p>
       <p>
-        <span className="font-normal">Precipitaciones: {data.clouds.all}</span>
+        <span className="font-normal">Precipitaciones: {clouds.all}</span>
       </p>
       <p>
-        <span className="font-normal">Viento:</span> {data.wind.speed} km/h
+        <span className="font-normal">Viento:</span> {wind.speed} km/h
       </p>
     </div>
   );
